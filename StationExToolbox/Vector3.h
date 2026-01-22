96fc0f8db2d2ef8173f245b6c099cfeba4150700
Michael Gray <54971998+michaeljgray@users.noreply.github.com>
2026-01-22 13:26:08 -0800

    Added formatter for Vector3.

--- StationExToolbox/Vector3.h
+++ StationExToolbox/Vector3.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <format>
 #include <string_view>
 
 using namespace std::string_view_literals;
@@ -17,3 +18,12 @@ namespace StationExToolbox
 		float Z;
 	};
 }
+
+template<> struct std::formatter<StationExToolbox::Vector3> : std::formatter<std::string>
+{
+	template<class FormatContext>
+	auto format(const StationExToolbox::Vector3& value, FormatContext& context) const
+	{
+		return std::formatter<std::string>::format(std::format("({}, {}, {})", value.X, value.Y, value.Z), context);
+	}
+};
